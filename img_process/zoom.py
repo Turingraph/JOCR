85a5f02658821dc44a81fe9582e13c39e0b84650
Turingraph <nemocheche@gmail.com>
2025-01-21 11:57:07 +0700

    img_process/zoom.py

--- img_process/zoom.py
+++ img_process/zoom.py
@@ -1,57 +1,54 @@
 import cv2
 import numpy as np
-from ImgProcess.Utility import GetSize
+from utility import get_size
 
 # https://github.com/wjbmattingly/ocr_python_textbook/blob/main/02_02_working%20with%20opencv.ipynb
 
-
-def RemoveBorders(img: np.ndarray):
+def remove_borders(img: np.ndarray) -> np.ndarray:
     contours, heiarchy = cv2.findContours(
-        img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        src=img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE
     )
-    cntsSorted = sorted(contours, key=lambda x: cv2.contourArea(x))
-    cnt = cntsSorted[-1]
-    x, y, w, h = cv2.boundingRect(cnt)
+    cnts_sorted = sorted(iterable=contours, key=lambda x: cv2.contourArea(x))
+    cnt = cnts_sorted[-1]
+    x, y, w, h = cv2.boundingRect(array=cnt)
     crop = img[y : y + h, x : x + w]
     return crop
 
-
-def Zoom(img: np.ndarray, zoom: int = 1):
+def zoom(img: np.ndarray, zoom: int = 1) -> np.ndarray:
     # https://stackoverflow.com/questions/69050464/zoom-into-image-with-opencv
     # zoom < 1 implies Zoom out
     # zoom > 1 implies Zoom in
     rot_mat = cv2.getRotationMatrix2D(
-        (img.shape[1] / 2, img.shape[0] / 2), 0, abs(zoom)
+        center=(img.shape[1] / 2, img.shape[0] / 2), angle=0, scale=abs(zoom)
     )
     result = cv2.warpAffine(
-        img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR
+        src=img, M=rot_mat, dst=img.shape[1::-1], flags=cv2.INTER_LINEAR
     )
     return result
 
 
-def CreateBorders(img: np.ndarray, size: int = 50):
+def create_borders(img: np.ndarray, size: int = 50) -> np.ndarray:
     top, bottom, left, right = [abs(size)] * 4
     img = cv2.copyMakeBorder(
-        img, top, bottom, left, right, cv2.BORDER_CONSTANT
-    )  # , value=color)
+        src=img, top=top, bottom=bottom, left=left, right=right, borderType=cv2.BORDER_CONSTANT
+    )
     return img
 
-
-def Crop(
+def crop(
     img: np.ndarray,
     x: int | None = None,
     y: int | None = None,
     width: int | None = None,
     height: int | None = None,
-):
-    x = GetSize(x, img.shape[1])
-    y = GetSize(y, img.shape[0])
+) -> np.ndarray:
+    x = get_size(size=x, max_size=img.shape[1])
+    y = get_size(size=y, max_size=img.shape[0])
     if type(width) == int:
-        width = GetSize(x + width, img.shape[1], img.shape[1] - x)
+        width = get_size(size=x + width, max_size=img.shape[1], default_size=img.shape[1] - x)
     else:
         width = img.shape[1]
     if type(height) == int:
-        height = GetSize(y + height, img.shape[0], img.shape[0] - y)
+        height = get_size(size=y + height, max_size=img.shape[0], default_size=img.shape[0] - y)
     else:
         height = img.shape[0]
     return img[y : y + height, x : x + width]
