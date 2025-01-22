2860938657be783dfdfe8f167f34b6fcfe95b651
Turingraph <nemocheche@gmail.com>
2025-01-22 18:46:28 +0700

    ignore tests/__fft/

---
+++ tests/__fft/img.py
@@ -0,0 +1,27 @@
+###############################################################################################################
+
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+parent = os.path.dirname(parent)
+sys.path.append(parent)
+
+###############################################################################################################
+
+from include.img_process_gray import img_process_gray
+from include.img_process_img import img_process_img
+
+path = parent + "/tests/fft/img/jojo_meme.jpg"
+img = img_process_img(img=path)
+img.show(title="color")
+img = img_process_gray(img=img.img)
+img.show(title="b&w")
+img.fft_sharp(row=100, col=100)
+img.show("sharp")
+# img.save("fft_sharp")
+
+"""
+python3 img.py
+"""
