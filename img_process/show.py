c91b3469ea89d479d5573ee06908bc53f1762129
Turingraph <nemocheche@gmail.com>
2025-01-23 14:56:31 +0700

    modify img_process/show.py/def save

--- img_process/show.py
+++ img_process/show.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 from PIL import Image
+from img_process.utility import get_options
 
 def show(img: np.ndarray, title: str = "img_out") -> None:
     # https://stackoverflow.com/questions/74546171/image-is-too-big-for-opencv-imshow-window-how-do-i-make-it-smaller
@@ -14,17 +15,62 @@ def show(img: np.ndarray, title: str = "img_out") -> None:
 
 def save(
     img: np.ndarray,
-    title: str = "img_out",
-    folder: str = "img_out",
-    fileformat: str = "jpg",
+    path: list[str] | str = ["img_out", "img_out", "jpg"],
 ) -> None:
     # https://stackoverflow.com/questions/902761/saving-a-numpy-array-as-an-image
-    if not os.path.exists(path=folder):
-        os.makedirs(name=folder)
-    if fileformat[0] == ".":
-        fileformat = fileformat[1:]
+    if isinstance(path, list):
+        if len(path) == 0:
+            path = ["img_out", "img_out", "jpg"]
+        if len(path) == 1:
+            path = [path[0], "img_out", "jpg"]
+        if len(path) == 2:
+            path = [path[0], path[1], "jpg"]
+    if isinstance(path, str):
+        path = [path, "img_out", "jpg"]
+
+    format_options = [
+        "jpg",
+        "jpeg",
+        "png",
+        "gif",
+        "bmp",
+        "tiff",
+        "ppm",
+        "ico",
+        "psd"
+    ]
+    if '.' == path[2][0]:
+        path[2][0] = path[2][0][1:]
+    message = """
+-------------------------------------------------------------------------------------------
+img_process/show.py/def save
+
+def save(
+    img: np.ndarray,
+    path: list[str] | str = ["img_out", "img_out", "jpg"],
+) -> None:
+# This function save the image
+Note that
+-   path[0] = folder_name
+-   path[1] = file_name
+-   path[2] = file_format
+
+available file_format options
+-   jpeg
+-   png
+-   gif
+-   bmp
+-   tiff
+-   ppm
+-   ico
+-   psd
+-------------------------------------------------------------------------------------------
+"""
+    path[2] = get_options(input=path[2], input_options=format_options, message=message)
+    if not os.path.exists(path=path[0]):
+        os.makedirs(name=path[0])
     # https://docs.python.org/3/library/os.path.html
-    path = os.path.join(folder, title + "." + fileformat)
+    path = os.path.join(path[0], path[1] + "." + path[2])
     # https://numpy.org/doc/2.1/reference/generated/numpy.save.html
     im = Image.fromarray(img)
     im.save(path)
